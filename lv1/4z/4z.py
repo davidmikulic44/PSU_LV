bd4100b0dc407afbeb4efb11635d9a2717e86462
David Mikulić <74313441+davidmikulic44@users.noreply.github.com>
2023-03-15 13:16:10 +0100

    Update 4z.py

--- lv1/4z/4z.py
+++ lv1/4z/4z.py
@@ -1,8 +1,10 @@
 ime_datoteke = input("Ime datoteke: ")
 dat_dir = "C:\\Users\\student\\Documents\\miki\\lv1\\4z\\"+ime_datoteke
 
-
-datoteka = open(dat_dir, 'r')
+try:
+    datoteka = open(dat_dir, 'r')
+except:
+    print("Datoteka ne postoji!")
 suma = 0
 brojac = 0
 
