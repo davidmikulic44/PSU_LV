e67927b75806509109bc422bda186925dd30ea43
David Mikulić <74313441+davidmikulic44@users.noreply.github.com>
2023-03-29 13:09:36 +0200

    Update 2z.py

--- lv2/zad2/2z.py
+++ lv2/zad2/2z.py
@@ -1,25 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.loadtxt(open("mtcars.csv", "rb"), usecols=(1,2,3,4,5,6),
-delimiter=",", skiprows=1)
+data = np.loadtxt(open("C:\\Users\\student\\Documents\\lv2\\zad2\\mtcars.csv", "rb"), usecols=(1, 2, 3, 4, 5, 6),
+                  delimiter=",", skiprows=1)
 
 
-print(data)
+print("min mpg: ", min(data[:, 0]))
+print("max mpg: ", max(data[:, 0]))
+print("avg mpg: ", sum(data[:, 0])/len(data[:, 0]))
 
-plt.scatter(data[:,0],data[:,3],c='b',s=data[:,5]*20)
+arr = data[:, 1] == 6
 
-print(min(data[:,0]))
-print(max(data[:,0]))
-print(sum(data[:,0])/len(data[:,0]))
-arr=[]
 
-for i,item in enumerate(data[:,1]):
-    if item >=6:
-        arr.append(data[i,0])
-        
-print("min: ", min(arr))
-print("max: ", max(arr))
-print("avg: ", sum(arr)/len(arr))
-    
+plt.scatter(data[:, 0], data[:, 3], c='lime',
+            ec='k', s=data[:, 5]*16, marker="h")
+
+for i, label in enumerate(data[:, 5]):
+    plt.text(data[i, 0], data[i, 3]+5, str(data[i, 5]))
+
+
+print("min mpg sa 6 cyl: ", min(data[arr, 0]))
+print("max mpg sa 6 cyl: ", max(data[arr, 0]))
+print("avg mpg sa 6 cyl: ", sum(data[arr, 0])/len(data[arr, 0]))
+
 plt.show()
