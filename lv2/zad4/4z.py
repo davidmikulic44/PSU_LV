400be2a74168070f395257dfc07a129e7ab1984f
David Mikulić <74313441+davidmikulic44@users.noreply.github.com>
2023-04-05 09:07:26 +0200

    Update 4z.py

--- lv2/zad4/4z.py
+++ lv2/zad4/4z.py
@@ -1,7 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
-check = np.zeros((4, 5))
-check[::2, 1::2] = 1
-check[1::2, ::2] = 1
+check = np.zeros((5, 5))
+
+redovi = len(check)
+stupci = len(check[0])
+
+for i in range(redovi):
+    for j in range(stupci):
+        if(i%2==0):
+            if(j%2!=0):
+                check[i][j]=1
+        if(i%2!=0):
+            if(j%2==0):
+                check[i][j]=1
+
 plt.matshow(check, cmap='gray')
 plt.show()
