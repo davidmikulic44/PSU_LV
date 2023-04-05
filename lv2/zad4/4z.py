9a7783894f701411697537af587adb0b70434e22
David Mikulić <74313441+davidmikulic44@users.noreply.github.com>
2023-04-05 09:41:21 +0200

    Update 4z.py

--- lv2/zad4/4z.py
+++ lv2/zad4/4z.py
@@ -1,22 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def checkers(redovi, stupci):
-    check = np.zeros((redovi, stupci))
-
-    for i in range(redovi):
-        for j in range(stupci):
-            if(i%2==0):
-                if(j%2!=0):
-                    check[i][j]=1
-            if(i%2!=0):
-                if(j%2==0):
-                    check[i][j]=1
+def check(kvadrat,redovi,stupci):
+    crne = np.zeros((kvadrat,kvadrat))
+    bijele = np.ones((kvadrat,kvadrat))*255
+    red1 = np.hstack([crne,bijele]*(stupci//2))
+    if stupci % 2 != 0:
+        red1 = np.hstack([red1,crne])
     
-    return check
+    red2 = np.hstack([bijele,crne]*(stupci//2))
+    if stupci % 2 != 0:
+        red2 = np.hstack([red2, bijele])
 
+    polje = np.vstack([red1,red2] * (redovi//2))
+    if redovi % 2 != 0:
+        polje = np.vstack([polje,red1])
+    return polje
 
-check = checkers(5,5)
-
-plt.matshow(check, cmap='gray')
+img = check(50,4,5)
+plt.imshow(img, cmap='gray', vmin=0, vmax=255)
 plt.show()
