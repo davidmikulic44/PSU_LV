ccbbdd96984a0c03ff3ff94b69c12a51a2edfe07
David Mikulić <74313441+davidmikulic44@users.noreply.github.com>
2023-04-05 09:14:46 +0200

    Update 4z.py

--- lv2/zad4/4z.py
+++ lv2/zad4/4z.py
@@ -1,18 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
-check = np.zeros((5, 5))
 
-redovi = len(check)
-stupci = len(check[0])
+def checkers(redovi, stupci):
+    check = np.zeros((redovi, stupci))
 
-for i in range(redovi):
-    for j in range(stupci):
-        if(i%2==0):
-            if(j%2!=0):
-                check[i][j]=1
-        if(i%2!=0):
-            if(j%2==0):
-                check[i][j]=1
+    for i in range(redovi):
+        for j in range(stupci):
+            if(i%2==0):
+                if(j%2!=0):
+                    check[i][j]=1
+            if(i%2!=0):
+                if(j%2==0):
+                    check[i][j]=1
+    
+    return check
+
+
+check = checkers(5,5)
 
 plt.matshow(check, cmap='gray')
 plt.show()
